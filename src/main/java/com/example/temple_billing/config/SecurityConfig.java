5d64587a44fbe5a2887a16b567701554683f8788
Abhilash C <abhilashc@Abhilashs-MacBook-Air.local>
2026-03-17 10:27:34 +0530

    config changes

--- src/main/java/com/example/temple_billing/config/SecurityConfig.java
+++ src/main/java/com/example/temple_billing/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200","https://temple-billing-service.onrender.com"));
+        config.setAllowedOrigins(List.of("http://localhost:4200","https://mankurussi-temple-billing-app.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
